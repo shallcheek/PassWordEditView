b6cd4aae76769c2cfd78cff71f808b204238cbb1
shallcheek <867030791@qq.com>
2016-07-19 14:28:30 +0800

    修改

--- library/src/main/java/com/chaek/android/widget/PassWordEditView.java
+++ library/src/main/java/com/chaek/android/widget/PassWordEditView.java
@@ -1,4 +1,4 @@
-package com.chaek.android.passwordeditview;
+package com.chaek.android.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -14,9 +14,9 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-/**
- * 密码输入
- */
+import com.chaek.android.R;
+
+
 public class PassWordEditView extends EditText implements TextWatcher {
     public static final int MAX_LEN = 12;
     public int password_max_len = 6;
@@ -40,7 +40,6 @@ public class PassWordEditView extends EditText implements TextWatcher {
     private int circleColor;
     private int circleFocusColor;
 
-    private float progress;
 
     /**
      * add password change listener
@@ -53,15 +52,13 @@ public class PassWordEditView extends EditText implements TextWatcher {
 
 
     /**
-     * @return 是否输入完
+     * @return edit is complete
      */
     public boolean isComplete() {
         return isComplete;
     }
 
-    /**
-     * @return 获取密码
-     */
+
     public String getPassword() {
         if (password == null)
             return "";
@@ -95,7 +92,6 @@ public class PassWordEditView extends EditText implements TextWatcher {
 
     @Override
     public void setInputType(int type) {
-        //重写默认格式的数字
         super.setInputType(EditorInfo.TYPE_CLASS_NUMBER);
     }
 
@@ -112,8 +108,6 @@ public class PassWordEditView extends EditText implements TextWatcher {
 
         rectF = new RectF();
 
-
-        //隐藏光标
         this.setCursorVisible(false);
         this.setFilters(new InputFilter[]{new InputFilter.LengthFilter(password_max_len)});
         setInputType(EditorInfo.TYPE_CLASS_NUMBER);
@@ -131,7 +125,6 @@ public class PassWordEditView extends EditText implements TextWatcher {
     public boolean dispatchTouchEvent(MotionEvent event) {
         int action = event.getAction();
         if (action == MotionEvent.ACTION_MOVE) {
-            //获取焦点弹出键盘
             setSelection(getText().toString().length());
             setFocusable(true);
             setFocusableInTouchMode(true);
@@ -139,8 +132,6 @@ public class PassWordEditView extends EditText implements TextWatcher {
             InputMethodManager inputManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             inputManager.showSoftInput(this, 0);
         }
-
-        //屏蔽
         return true;
 
     }
@@ -168,9 +159,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 圆的颜色
-     *
-     * @param circleColor 颜色
+     * @param circleColor password circle background color
      */
     public void setCircleColor(int circleColor) {
         this.circleColor = circleColor;
@@ -178,9 +167,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 背景颜色
-     *
-     * @param background 颜色
+     * @param background view background color
      */
     public void setBackground(int background) {
         backgroundColor = background;
@@ -188,9 +175,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 控件获取焦点后的颜色
-     *
-     * @param circleFocusColor 颜色
+     * @param circleFocusColor password circle focus background color
      */
     public void setCircleFocusColor(int circleFocusColor) {
         this.circleFocusColor = circleFocusColor;
@@ -198,9 +183,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 线条和边线颜色
-     *
-     * @param lineColor 颜色
+     * @param lineColor line color
      */
     public void setLineColor(int lineColor) {
         this.lineColor = lineColor;
@@ -208,9 +191,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 背景圆角大小
-     *
-     * @param passwordRadius 大小
+     * @param passwordRadius view background radius
      */
     public void setPasswordRadius(int passwordRadius) {
         this.passwordRadius = passwordRadius;
@@ -218,9 +199,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 控件获取焦点后 线条和边线颜色
-     *
-     * @param lineFocusColor 颜色
+     * @param lineFocusColor this is focus line color
      */
     public void setLineFocusColor(int lineFocusColor) {
         this.lineFocusColor = lineFocusColor;
@@ -228,9 +207,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 密码圆点的大小
-     *
-     * @param circleSize 大小
+     * @param circleSize password circle size
      */
     public void setCircleSize(int circleSize) {
         this.circleSize = circleSize;
@@ -238,9 +215,7 @@ public class PassWordEditView extends EditText implements TextWatcher {
     }
 
     /**
-     * 输入的最大长度
-     *
-     * @param password_max_len 密码长度
+     * @param password_max_len edit view input max len
      */
     public void setMaxLen(int password_max_len) {
         this.password_max_len = password_max_len;
@@ -254,11 +229,9 @@ public class PassWordEditView extends EditText implements TextWatcher {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //背景
         passwordPaint.setColor(backgroundColor);
         passwordPaint.setStyle(Paint.Style.FILL);
         canvas.drawRoundRect(rectF, passwordRadius, passwordRadius, passwordPaint);
-        //边框颜色
 
         if (isFocused()) {
             passwordPaint.setColor(lineFocusColor);
@@ -271,21 +244,18 @@ public class PassWordEditView extends EditText implements TextWatcher {
 
         int w = width / password_max_len;
         int h = getHeight() / 2;
-        //分割线
         passwordPaint.setStrokeWidth(1f);
         passwordPaint.setStyle(Paint.Style.FILL);
 
         for (int i = 0; i < password_max_len - 1; i++) {
             canvas.drawLine(i * w + w, 1, i * w + w, height, passwordPaint);
         }
-        //圆点颜色
         if (isFocusable()) {
             passwordRoundPaint.setColor(circleFocusColor);
         } else {
             passwordRoundPaint.setColor(circleColor);
         }
 
-        //密码圆点
         for (int i = 0; i < passWordCount; i++) {
             canvas.drawCircle(i * w + w / 2, h, circleSize, passwordRoundPaint);// 小圆
         }
@@ -301,8 +271,8 @@ public class PassWordEditView extends EditText implements TextWatcher {
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         password = s.toString();
         passWordCount = s.length();
-        //完成
         isComplete = passWordCount == password_max_len;
+
         if (passWordChangeListener != null) {
             passWordChangeListener.onTextChange();
             if (isComplete()) {
